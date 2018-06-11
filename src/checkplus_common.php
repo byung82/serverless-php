9afb76de29b638dbdf02a759d58fe9743088a51c
Byung Seok Kim <byung82@kiwiworks.kr>
2018-06-11 17:19:05 +0900

    checkplus 추가

---
+++ src/checkplus_common.php
@@ -0,0 +1,14 @@
+<?php
+
+$sitecode = "AC443";				// NICE로부터 부여받은 사이트 코드
+$sitepasswd = "Q0eCUaO7Ye88";			// NICE로부터 부여받은 사이트 패스워드
+
+
+$module = 'CPClient';
+if (!extension_loaded($module)) {
+
+  die("No module loaded...");
+}
+
+
+?>
